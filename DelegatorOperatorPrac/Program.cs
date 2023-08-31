5cfd521933778b552a42a94f23612fa9ffd26e3d
Nagyeong Park <zzum0701@naver.com>
2023-08-31 15:10:44 +0900

    델리게이터 덧셈 뺄셈

--- DelegatorOperatorPrac/Program.cs
+++ DelegatorOperatorPrac/Program.cs
@@ -8,9 +8,26 @@ namespace DelegatorOperatorPrac
 {
     internal class Program
     {
+        public delegate void SendString(string message);
         static void Main(string[] args)
         {
+            SendString sayHello, sayGoodbye, multiDelegate;
+            sayHello = Hello;
+            sayGoodbye = GoodBye;
+            multiDelegate = sayHello + sayGoodbye;
+            multiDelegate("박나경");
+            multiDelegate -= sayGoodbye;
+            multiDelegate("박나경");
+        }
 
+        private static void GoodBye(string message)
+        {
+            Console.WriteLine("안녕히가세요, " + message + "님");
+        }
+
+        private static void Hello(string message)
+        {
+            Console.WriteLine("안녕하세요, " + message + "님");
         }
     }
 }
